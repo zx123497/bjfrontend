d9790339e5a97083ae7a55e79dd621cd6a934c2b
yww1327 <yww1327@gmail.com>
2021-04-08 17:57:52 +0800

    socket service build and test

--- src/pages/Backstage/GameLobby/GameLobby.js
+++ src/pages/Backstage/GameLobby/GameLobby.js
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { withStyles } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core';
 import AnnouncementLine from '../../../components/ForGameLobby/AnnouncementLine'
 import UpperBar from '../../../components/ForGameLobby/UpperBar'
 import GameChart from '../../../components/ForGameLobby/GameChart'
 import TransRecord from '../../../components/ForGameLobby/TransRecord'
-
+import { socket } from '../../../service/socket'
 const useStyles = makeStyles((theme) => ({
     root: {
+
     }
 }));
 
 
 const GameLobby = (props) => {
+
+    //////////////////////////////
+    //        sockect try       //
+    //////////////////////////////
+
+    const [connected, setConnected] = useState(false);
+
+    useEffect(() => {
+        socket.emit('test');
+        socket.on('testResponse', obj => {
+            console.log(obj);
+        });
+        // unsubscribe from event for preventing memory leaks
+    }, []);
+
+    console.log(socket);
+
+    //////////////////////////////
 
     const classes = useStyles();
 
