22e961570c299765eda8eb07b0f7fc8cc13f13df
ally0108 <allyhappy@g.ncu.edu.tw>
2021-04-22 16:28:10 +0800

    commit for pull

--- src/pages/Backstage/GameLobby/GameLobby.js
+++ src/pages/Backstage/GameLobby/GameLobby.js
@@ -1,44 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import { withStyles } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core';
-import AnnouncementLine from '../../../components/ForGameLobby/AnnouncementLine'
+import React from 'react'
+import { makeStyles } from '@material-ui/core'
+import { withRouter } from 'react-router-dom';
 import UpperBar from '../../../components/ForGameLobby/UpperBar'
-import GameChart from '../../../components/ForGameLobby/GameChart'
-import TransRecord from '../../../components/ForGameLobby/TransRecord'
-import { socket } from '../../../service/socket'
+import AnnouncementLine from '../../../components/ForGameLobby/AnnouncementLine'
+import UserInfo from '../../../components/ForGameLobby/UserInfo'
+import PersonalTransaction from '../../../components/ForGameLobby/PersonalTransaction'
+
 const useStyles = makeStyles((theme) => ({
     root: {
         paddingTop: "35px"
-    },
-    componenet: {
-        width: "100vw",
-        height: "55vh",
-        display: "flex",
-        justifyContent: "center",
-        marginTop: theme.spacing(3)
     }
 }));
 
-
 const GameLobby = (props) => {
-
-    //////////////////////////////
-    //        sockect try       //
-    //////////////////////////////
-
-    const [connected, setConnected] = useState(false);
-
-    useEffect(() => {
-        socket.emit('test');
-        socket.on('testResponse', obj => {
-            console.log(obj);
-        });
-        // unsubscribe from event for preventing memory leaks
-    }, []);
-
-    console.log(socket);
-
-    //////////////////////////////
 
     const classes = useStyles();
 
@@ -46,12 +20,10 @@ const GameLobby = (props) => {
         <div className={classes.root}>
             <UpperBar />
             <AnnouncementLine />
-            <div className={classes.componenet}>
-                <GameChart />
-                <TransRecord />
-            </div>
+            <UserInfo />
+            <PersonalTransaction />
         </div>
     )
 }
 
-export default GameLobby
+export default withRouter(GameLobby)
