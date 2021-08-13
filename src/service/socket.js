37b7e9e281bbb5d39baae82ad1317640d1bef100
leo <leo000111444@gmail.com>
2021-08-13 23:37:42 +0800

    https

--- src/service/socket.js
+++ src/service/socket.js
@@ -1,18 +1,17 @@
-import io from "socket.io-client";
-const URL = "http://140.115.83.113:8080";
+import io from 'socket.io-client'
+const URL = 'https://lbdgame.mgt.ncu.edu.tw:8080'
 
 export const socket = io(URL, {
-    transports: ['websocket', 'polling', 'flashsocket']
-});
-
+    transports: ['websocket', 'polling', 'flashsocket'],
+})
 
 // import { socket } from '../../../service/socket'
 
 // const [connected, setConnected] = useState(false);
 
 // useEffect(() => {
-    // socket.emit('test');
-    // socket.on('testResponse', obj => {
-    //     console.log(obj);
-    // });
+// socket.emit('test');
+// socket.on('testResponse', obj => {
+//     console.log(obj);
+// });
 // }, []);
