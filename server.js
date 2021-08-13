194ade2ce0b63157234ca979e8e1f242ab863b38
leo <leo000111444@gmail.com>
2021-08-13 11:45:20 +0800

    https

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/ping', function (req, res) {
     return res.send('pong')
 })
 app.get('/*', function (req, res) {
-    res.sendFile(path.join('build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 app.listen(port)
