48075ec8fd8409ee59ec553e6bad750d364f1752
leo <leo000111444@gmail.com>
2021-09-24 00:41:07 +0800

    fix routing scope

--- src/App.js
+++ src/App.js
@@ -103,56 +103,7 @@ function App() {
     }, [dispatch])
 
     const appliedTheme = Theme
-    if (!loginState.isLogin) {
-        return (
-            <AuthContext.Provider value={authContext}>
-                <div className={classes.root}>
-                    <ThemeProvider theme={appliedTheme}>
-                        <div className="App">
-                            <Navbar />
-                            <Switch>
-                                <Route
-                                    path="/"
-                                    render={() => (
-                                        <>
-                                            <main>
-                                                <AnimatePresence exitBeforeEnter>
-                                                    <Switch>
-                                                        <Route path="/" exact component={LogIn} />
-                                                        <Route
-                                                            path="/user/edit/password"
-                                                            exact
-                                                            component={ForgetPassword}
-                                                        />
-                                                        <Route path="/login" exact component={LogIn} />
-                                                        <Route path="/register" exact component={Register} />
-                                                        <Route path="/register2" exact component={Register2} />
-                                                        <Route
-                                                            path="/forgetpassword"
-                                                            exact
-                                                            component={ForgetPassword}
-                                                        />
-                                                        <Route
-                                                            path="/forgetpassword2"
-                                                            exact
-                                                            component={ForgetPassword2}
-                                                        />
-                                                        <Route path="/resetpassword" exact component={ResetPassword} />
-                                                        <Route path="/" component={NotFound} />
-                                                    </Switch>
-                                                </AnimatePresence>
-                                            </main>
-                                        </>
-                                    )}
-                                />
-                                <Route path="/" component={NotFound} />
-                            </Switch>
-                        </div>
-                    </ThemeProvider>
-                </div>
-            </AuthContext.Provider>
-        )
-    }
+
     return (
         <AuthContext.Provider value={authContext}>
             <div className={classes.root}>
@@ -169,7 +120,7 @@ function App() {
                                             <AnimatePresence exitBeforeEnter>
                                                 <Switch>
                                                     <Route path="/" exact component={LogIn} />
-                                                    <Route path="/user/lobby" exact component={UserLobby} />
+
                                                     <Route
                                                         path="/user/edit/password"
                                                         exact
@@ -178,17 +129,23 @@ function App() {
                                                     <Route path="/login" exact component={LogIn} />
                                                     <Route path="/register" exact component={Register} />
                                                     <Route path="/register2" exact component={Register2} />
-
                                                     <Route path="/forgetpassword" exact component={ForgetPassword} />
                                                     <Route path="/forgetpassword2" exact component={ForgetPassword2} />
-                                                    <Route path="/resetpassword" exact component={ResetPassword} />
-                                                    <Route path="/loading/:id" exact component={Loading} />
-                                                    <Route path="/gamelobby/:id" exact component={GameLobby} />
-                                                    <Route path="/gamein" exact component={GameIn} />
-                                                    <Route path="/qrcode" exact component={QRCodeSend2} />
-                                                    <Route path="/teacherqrcode" exact component={QRCodeSend} />
+                                                    {loginState.isLogin && (
+                                                        <>
+                                                            <Route path="/user/lobby" exact component={UserLobby} />
+                                                            <Route path="/loading/:id" exact component={Loading} />
+                                                            <Route path="/gamelobby/:id" exact component={GameLobby} />
+                                                            <Route path="/gamein" exact component={GameIn} />
+                                                            <Route path="/qrcode" exact component={QRCodeSend2} />
+                                                        </>
+                                                    )}
+
                                                     {loginState.isAdmin && (
-                                                        <Route path="/admin" component={BackStage} />
+                                                        <>
+                                                            <Route path="/admin" component={BackStage} />
+                                                            <Route path="/teacherqrcode" exact component={QRCodeSend} />
+                                                        </>
                                                     )}
                                                     <Route path="/" component={NotFound} />
                                                 </Switch>
