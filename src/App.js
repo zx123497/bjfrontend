09be8d02e98956bbe513ef13db1508a5899c7c25
leo <leo000111444@gmail.com>
2021-09-23 21:19:23 +0800

    fix bugs

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
     useEffect(() => {
         if (localStorage.getItem('token')) {
             setIsLogin(true)
-        } else if (localStorage.getItem('isAdmin')) {
+        } else if (localStorage.getItem('isAdmin') === '1') {
             setIsAdmin(true)
         } else {
             setIsLogin(false)
@@ -130,7 +130,7 @@ function App() {
                                                 <Route path="/gamein" exact component={GameIn} />
                                                 <Route path="/qrcode" exact component={QRCodeSend2} />
                                                 <Route path="/teacherqrcode" exact component={QRCodeSend} />
-                                                {isAdmin === '1' && <Route path="/admin" exact component={BackStage} />}
+                                                {isAdmin && <Route path="/admin" exact component={BackStage} />}
                                                 <Route path="/" component={NotFound} />
                                             </Switch>
                                         </AnimatePresence>
