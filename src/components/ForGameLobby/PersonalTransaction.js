3efd6caa9b3b2812141ad04fe08986fe898e8bdc
ally0108 <allyhappy@gmail.com>
2021-09-23 21:39:05 +0800

    forgamelobby

--- src/components/ForGameLobby/PersonalTransaction.js
+++ src/components/ForGameLobby/PersonalTransaction.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Grid, makeStyles, Typography } from '@material-ui/core';
-import { withRouter } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { Box, Grid, makeStyles, Typography } from '@material-ui/core'
+import { withRouter } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -9,62 +9,61 @@ const useStyles = makeStyles((theme) => ({
     box: {
         padding: theme.spacing(1),
         borderRadius: theme.spacing(3),
-        textAlign: "center",
+        textAlign: 'center',
         backgroundColor: theme.palette.ultimate.main,
-        color: "white"
+        color: 'white',
     },
     container: {
-        width: "80%",
-        marginLeft: "10%",
-        "& .row": {
-            marginTop: theme.spacing(4)
+        width: '80%',
+        marginLeft: '10%',
+        '& .row': {
+            marginTop: theme.spacing(4),
         },
-        "& .cell": {
-            textAlign: "center",
-            color: "white"
+        '& .cell': {
+            textAlign: 'center',
+            color: 'white',
         },
-        "& .caption": {
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
+        '& .caption': {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
         },
-        "& .partnerInfo": {
-            width: "100%",
-            display: "flex",
-            justifyContent: "center"
+        '& .partnerInfo': {
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
         },
-        "& .nameContainer": {
-            fontSize:"1.3rem",
-            height: "4rem",
-            lineHeight: "4rem",
-            marginLeft: theme.spacing(2)
-        }
-    }
-}));
+        '& .nameContainer': {
+            fontSize: '1.3rem',
+            height: '4rem',
+            lineHeight: '4rem',
+            marginLeft: theme.spacing(2),
+        },
+    },
+}))
 
 const PersonalTransaction = (props) => {
-
     console.log(props.data)
 
-    const [ trans, setTrans ] = useState({
+    const [trans, setTrans] = useState({
         score: '',
         transPartner: '',
-        tranAmount: ''
+        tranAmount: '',
     })
 
     useEffect(() => {
-        if(localStorage.getItem(`tran${props.data.room.round}_money`)) {
-            if(props.data.player.role == "seller") {
+        if (localStorage.getItem(`tran${props.data.room.round}_money`) != null) {
+            if (props.data.player.role == 'seller') {
                 setTrans({
                     score: props.data.player.score,
                     transPartner: localStorage.getItem(`tran${props.data.room.round}_user`),
-                    tranAmount: localStorage.getItem(`tran${props.data.room.round}_money`)
+                    tranAmount: localStorage.getItem(`tran${props.data.room.round}_money`),
                 })
             } else {
                 setTrans({
                     score: props.data.player.score,
                     transPartner: localStorage.getItem(`tran${props.data.room.round}_user`),
-                    tranAmount: localStorage.getItem(`tran${props.data.room.round}_money`)
+                    tranAmount: localStorage.getItem(`tran${props.data.room.round}_money`),
                 })
             }
         } else {
@@ -72,78 +71,55 @@ const PersonalTransaction = (props) => {
                 score: 0,
                 totalScore: 0,
                 transPartner: '',
-                tranAmount: 0
+                tranAmount: 0,
             })
         }
     }, [props.data])
 
-    const classes = useStyles();
+    const classes = useStyles()
 
     return (
         <div className={classes.root}>
-            <Box className={classes.box}>
-                交易紀錄與得分總計
-            </Box>
+            <Box className={classes.box}>交易紀錄與得分總計</Box>
             <Grid container className={classes.container}>
                 <Grid container className="row pointSum" justify="center">
                     <Grid item xs={4} className="cell caption">
-                        <Typography variant="subtitle">
-                            得分總計：
-                        </Typography>
+                        <Typography variant="subtitle">得分總計：</Typography>
                     </Grid>
                     <Grid item xs={8} className="cell">
-                        {trans.score >= 0 && (
-                            <Typography variant="h4">
-                                +${trans.score}
-                            </Typography>
-                        )}
+                        {trans.score >= 0 && <Typography variant="h4">+${trans.score}</Typography>}
 
-                        {trans.score < 0 && (
-                            <Typography variant="h4">
-                                -${Math.abs(trans.score)}
-                            </Typography>
-                        )}
+                        {trans.score < 0 && <Typography variant="h4">-${Math.abs(trans.score)}</Typography>}
                     </Grid>
                 </Grid>
 
                 <Grid container className="row partner" justify="center">
                     <Grid item xs={4} className="cell caption">
-                        <Typography variant="subtitle">
-                            交易對象：
-                        </Typography>
+                        <Typography variant="subtitle">交易對象：</Typography>
                     </Grid>
                     <Grid item xs={8} className="cell">
                         <div className="partnerInfo">
-                            <div className="nameContainer">
-                                {trans.transPartner}
-                            </div>
+                            <div className="nameContainer">{trans.transPartner}</div>
                         </div>
                     </Grid>
                 </Grid>
 
                 <Grid container className="row partner" justify="center">
                     <Grid item xs={4} className="cell caption">
-                        <Typography variant="subtitle">
-                            交易點數：
-                        </Typography>
+                        <Typography variant="subtitle">交易點數：</Typography>
                     </Grid>
                     <Grid item xs={8} className="cell">
                         {props.data.player.role == 'seller' && (
-                            <Typography variant="h5">
-                                + ${trans.tranAmount}
-                            </Typography>
+                            <Typography variant="h5">+ ${trans.tranAmount}</Typography>
                         )}
 
                         {props.data.player.role == 'buyer' && (
-                            <Typography variant="h5">
-                                - ${Math.abs(trans.tranAmount)}
-                            </Typography>
+                            <Typography variant="h5">- ${Math.abs(trans.tranAmount)}</Typography>
                         )}
                     </Grid>
                 </Grid>
-
             </Grid>
-        </div >
+        </div>
     )
 }
 
